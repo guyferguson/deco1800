e9cbef6179d8d8d34695480b2560a271dced67da
guyferguson <guyferguson@tpg.com.au>
2016-08-30 23:33:22 +1000

    urlRewrites, year from range
    loc site, archive-collection, plus getting year rfom a range (1858-1860)

--- js/trove.js
+++ js/trove.js
@@ -8,7 +8,7 @@
     var imageYears = [];
 
 
-    var urlPatterns = ["flickr.com", "nla.gov.au", "artsearch.nga.gov.au", "recordsearch.naa.gov.au", "images.slsa.sa.gov.au", "collections.museumvictoria.com.au"];
+    var urlPatterns = ["flickr.com", "nla.gov.au", "artsearch.nga.gov.au", "recordsearch.naa.gov.au", "images.slsa.sa.gov.au", "collections.museumvictoria.com.au","archival-classic.sl.nsw.gov.au","handle.slv.vic.gov.au","hdl.loc.gov"];
 
     var found = 0;
 
@@ -16,7 +16,7 @@
 
 	function waitForFlickr() {
 
-		if(found == loadedImages.length) {
+		if(found === loadedImages.length) {
 
 			printImages();
 
@@ -88,21 +88,33 @@
     function processImages(index, troveItem) {
 
         var imgUrl = troveItem.identifier[0].value;
-        var imgYear = troveItem.issued;
-		
-		//Determine if the year is a valid year 
-		
-		if (imgYear > 1500 && imgYear < 2100)  {
-        // console.log(imgYear + " is the year of the image");
+		var imgYear;
+		//Determine if there is a date tag in JSON 
+		if (troveItem.hasOwnProperty('issued')) {
+        	imgYear = String(troveItem.issued);
+			console.log("About to work with " + imgYear + " on " + imgUrl);
+			console.log("converting imgYear into " + imgYear.substring(0,4));
+			if (Number(imgYear) > 1500 && Number(imgYear) < 2100)  {
+			// console.log(imgYear + " is the year of the image");
+			} else if ((Number(imgYear.substring(0,4)) > 1500 && Number(imgYear.substring(0,4) < 2100))){
+			 console.log("converting imgYear into " + imgYear.substring(0,4));
+			 imgYear = imgYear.substring(0,4);  // Set it to 9999, then we include 9999 with every generation
+			} else {
+			console.log(imgYear + " is not a valid year");	
+			 imgYear = "No Date";  // Set it to 9999, then we include 9999 with every generation
+			}
 		} else {
-		// console.log(imgYear + " is not a valid year");
-		 imgYear = "No Date";  // Set it to 9999, then we include 9999 with every generation
+			console.log("No year stored for " + imgUrl);
+			imgYear = "No Date";
 		}
+		 console.log("This one needs to match: ");
+		 console.log(imgUrl);
 		
+		var path;		  
 	    var searchYear = $("#searchYear").val().trim();
 		
-		if (imgYear == "No Date" || ((Number(imgYear) > Number(searchYear) - 11) && (Number(imgYear) < Number(searchYear) + 11))) {
-			console.log("This pic is  " + imgYear + " which is near " + searchYear); 
+		if (imgYear === "No Date" || ( (Number(imgYear) > Number(searchYear) - 11) && (Number(imgYear) < Number(searchYear) + 11) )) {
+			//console.log("This pic is  " + imgYear + " which is near " + searchYear); 
 			  if (imgUrl.indexOf(urlPatterns[0]) >= 0) { // flickr
 	  
 			  found++;
@@ -176,7 +188,58 @@
 			  );
 				  imageYears.push(imgYear);
 	  
+			  } else if (imgUrl.indexOf(urlPatterns[6]) >= 0) { // archival-classic.sl.nsw.gov.au
 	  
+				  found++;
+//				  console.log(troveItem  );
+				  console.log("FOUND AN ARCHIVAL: ");
+		//		  console.log(troveItem.identifier[1].value);
+	  
+				  loadedImages.push(
+	  
+					 troveItem.identifier[0].value
+	  
+			  );
+				  imageYears.push(imgYear);
+	  
+			  }  else if (imgUrl.indexOf(urlPatterns[7]) >= 0) { // handle.slv.vic.gov.au
+	  
+				  found++;
+	//			  console.log(troveItem  );
+				  console.log("FOUND AN HDL VIC GOV: ");
+		//		  console.log(troveItem.identifier[1].value);
+	  			  
+				 	//Hackish way to determine if there is an item in identifier[1]
+	  				if (troveItem.identifier.length > 1) {
+						console.log("Using identifier[1]");
+						path = troveItem.identifier[1].value;
+					} else {
+						console.log("Using identifier[0]");
+						// This weill need changing, but no idea where to get the 3c2000 stuff from...
+						path = "http://cdn.loc.gov/service/pnp/cph/3c20000/3c24000/3c24100/" + imgUrl.substring(String(troveItem.identifier[0].value).length-7,String(troveItem.identifier[0].value).length);
+						console.log("Going to path " + path);
+					}
+				  loadedImages.push(path);
+				  imageYears.push(imgYear);
+	  
+			  }  else if (imgUrl.indexOf(urlPatterns[8]) >= 0) { // hdl.loc.gov
+	  
+				  found++;
+	//			  console.log(troveItem  );
+				  console.log("FOUND AN HDL LOC GOV: ");
+		//		  console.log(troveItem.identifier[1].value);
+					if (troveItem.identifier.length > 1) {
+						path = troveItem.identifier[1].value;
+					} else {
+				  path = "http://cdn.loc.gov/service/pnp/cph/3c20000/3c24000/3c24100/" + imgUrl.substring(String(troveItem.identifier[0].value).length-7,String(troveItem.identifier[0].value).length) + "r.jpg";
+					}
+						console.log("Going to path " + path);
+				  loadedImages.push(
+	  
+					 path
+	  
+			  );
+				  imageYears.push(imgYear);
 	  
 			  }
 	  
