007eb8ce37556b481d50a1dd4448f415edbdd87b
guyferguson <guyferguson@tpg.com.au>
2016-09-28 06:44:56 -0700

    Initial Virtbox

--- js/trove.js
+++ js/trove.js
@@ -3,39 +3,31 @@
 // Added a reference on index.php:
 //
 //         <script src="js/trove.js">
-
-    var loadedImages = [];
-    var imageYears = [];
-
-
-    var urlPatterns = ["flickr.com", "nla.gov.au", "artsearch.nga.gov.au", "recordsearch.naa.gov.au", "images.slsa.sa.gov.au", "collections.museumvictoria.com.au","archival-classic.sl.nsw.gov.au","handle.slv.vic.gov.au","hdl.loc.gov"];
-
-    var found = 0;
+ 	   var loadedImages = [];
+       var urlPatterns = ["flickr.com", "nla.gov.au", "artsearch.nga.gov.au", "recordsearch.naa.gov.au", "images.slsa.sa.gov.au", "collections.museumvictoria.com.au","archival-classic.sl.nsw.gov.au","handle.slv.vic.gov.au","hdl.loc.gov"];
+       var found = 0;
 
 (function($){
 
 	function waitForFlickr() {
+	console.log("wait flickr Found = " + found+ "loadedImagesLength = " + loadedImages.length);
 
-		if(found === loadedImages.length) {
+	 	if(found === loadedImages.length) {
 
+        $("#output").append("<h3>Image Search Results</h3>");
 			printImages();
-
 		} else {
-
 			setTimeout(waitForFlickr, 250);
-
 		}
-
 	}
 
+
     $("form#searchTrove").submit(function(event) {
+		loadedImages.length = 0;  //Clear the array
+		found = 0;
+     	console.log("startSearch Found = " + found);
 
         event.preventDefault();
-        loadedImages = [];
-		imageYears = [];
-
-		found = 0;
-
         //get input values
         var searchTerm = $("#searchTerm").val().trim();
 
@@ -43,17 +35,11 @@
 
         searchTerm = searchTerm + "%20portrait";    //200816 FFT addition to return human images
 
-        //var sortBy = $("#sortBy").val();
-
         var apiKey = "bjta6lbqejkmpk9k";
-
-
-
-        //create searh query
+		
+        //create search query
 
         var url = "http://api.trove.nla.gov.au/result?key=" + apiKey + "&l-availability=y%2Ff&encoding=json&zone=picture" + "&sortby=relevance&n=100&q=" + searchTerm + "&callback=?";
-
-
 
         //get the JSON information we need to display the images
 
@@ -65,11 +51,7 @@
 
             $.each(data.response.zone[0].records.work, processImages);
 
-            //printImages();
-
-
-
-	   waitForFlickr(); // Waits for the flickr images to load
+	  		 waitForFlickr(); // Waits for the flickr images to load
 
         });
 
@@ -89,6 +71,7 @@
 
         var imgUrl = troveItem.identifier[0].value;
 		var imgYear;
+	
 		//Determine if there is a date tag in JSON 
 		if (troveItem.hasOwnProperty('issued')) {
         	imgYear = String(troveItem.issued);
@@ -107,139 +90,98 @@
 			console.log("No year stored for " + imgUrl);
 			imgYear = "No Date";
 		}
-		 console.log("This one needs to match: ");
-		 console.log(imgUrl);
-		
+        var imageData = [];
 		var path;		  
-	    var searchYear = $("#searchYear").val().trim();
 		
-		if (imgYear === "No Date" || ( (Number(imgYear) > Number(searchYear) - 11) && (Number(imgYear) < Number(searchYear) + 11) )) {
-			//console.log("This pic is  " + imgYear + " which is near " + searchYear); 
+
 			  if (imgUrl.indexOf(urlPatterns[0]) >= 0) { // flickr
-	  
-			  found++;
-	  
-				  addFlickrItem(imgUrl, troveItem, imgYear);
-	  
-	  
-	  
+	  		      found++;
+	  			  addFlickrItem(imgUrl, troveItem, imgYear);
+	  	  
 			  } else if (imgUrl.indexOf(urlPatterns[1]) >= 0) { // nla.gov
 	  
-			  found++;
-	  
-				  loadedImages.push(
-	  
-					  imgUrl + "/representativeImage?wid=900" // change ?wid=900 to scale the image
-	  
-				  );
-				  imageYears.push(imgYear);
-	  
+		   	      found++;
+	              imageData[0] = imgYear;
+				  imageData[1] =  imgUrl + "/representativeImage?wid=900"; // change ?wid=900 to scale the image
+				  imageData[2] =  troveItem.title;
+		 	      loadedImages.push(imageData);
 	  
 			  } else if (imgUrl.indexOf(urlPatterns[2]) >= 0) { //artsearch
 	  
-			  found++;
-	  
-				  loadedImages.push(
-	  
-					  "http://artsearch.nga.gov.au/IMAGES/LRG/" + getQueryVariable("IRN", imgUrl) + ".jpg"
-	  
-				  );
-				  imageYears.push(imgYear);
-	  
-	  
-	  
+		   	       found++;
+	               imageData[0] = imgYear;
+		  		   imageData[1] =    "http://artsearch.nga.gov.au/IMAGES/LRG/" + getQueryVariable("IRN", imgUrl) + ".jpg";
+				  imageData[2] =  troveItem.title;
+				   loadedImages.push(imageData);
+	    
 			  } else if (imgUrl.indexOf(urlPatterns[3]) >= 0) { //recordsearch
 	  
-			  found++;
-	  
-				  loadedImages.push(
-	  
-					  "http://recordsearch.naa.gov.au/NAAMedia/ShowImage.asp?T=P&S=1&B=" + getQueryVariable("Number", imgUrl)
-	  
-				  );
-				  imageYears.push(imgYear);
-	  
-	  
+			  	   found++;
+	               imageData[0] = imgYear;
+		  		   imageData[1] =   "http://recordsearch.naa.gov.au/NAAMedia/ShowImage.asp?T=P&S=1&B=" + getQueryVariable("Number", imgUrl);
+				   imageData[2] =  troveItem.title;
+				   loadedImages.push(imageData);
 	  
 			  } else if (imgUrl.indexOf(urlPatterns[4]) >= 0) { //slsa
 	  
-				  found++;
-	  
-				  loadedImages.push(
-	  
-					  imgUrl.slice(0, imgUrl.length - 3) + "jpg"
-	  
-			  );
-				  imageYears.push(imgYear);
-	  
-	  
-	  
+				   found++;
+	  			   imageData[0] = imgYear;
+		  		   imageData[1] =    imgUrl.slice(0, imgUrl.length - 3) + "jpg";
+				   imageData[2] =  troveItem.title;
+				   loadedImages.push(imageData);
 			  } else if (imgUrl.indexOf(urlPatterns[5]) >= 0) { // collections.museumvictoria.com.au
 	  
 				  found++;
-				  //console.log(troveItem  );
-				  //console.log("We should perhaps go to: ");
-				  //console.log(troveItem.identifier[1].value);
-	  
-				  loadedImages.push(
-	  
-					 troveItem.identifier[1].value
-	  
-			  );
-				  imageYears.push(imgYear);
+				   imageData[0] = imgYear;
+		  		   imageData[1] =     troveItem.identifier[1].value;
+				   imageData[2] =  troveItem.title;
+				   loadedImages.push(imageData);
 	  
 			  } else if (imgUrl.indexOf(urlPatterns[6]) >= 0) { // archival-classic.sl.nsw.gov.au
 	  
-				  found++;
-//				  console.log(troveItem  );
-				  console.log("FOUND AN ARCHIVAL: ");
-		//		  console.log(troveItem.identifier[1].value);
-	  
-				  loadedImages.push(
-	  
-					 troveItem.identifier[0].value
-	  
-			  );
-				  imageYears.push(imgYear);
+				   found++;
+			   	   imageData[0] = imgYear;
+		  		   imageData[1] =   troveItem.identifier[0].value;
+				   imageData[2] =  troveItem.title;
+				   loadedImages.push(imageData);
 	  
 			  }  else if (imgUrl.indexOf(urlPatterns[7]) >= 0) { // handle.slv.vic.gov.au
 	  
 				  found++;
-	//			  console.log(troveItem  );
-				  console.log("FOUND AN HDL VIC GOV: ");
-		//		  console.log(troveItem.identifier[1].value);
-	  			  
+			//	  console.log("FOUND AN SLV VIC GOV: ");
 				 	//Hackish way to determine if there is an item in identifier[1]
 	  				if (troveItem.identifier.length > 1) {
-						console.log("Using identifier[1]");
+			//			console.log("Using identifier[1]");
 						path = troveItem.identifier[1].value;
 					} else {
-						console.log("Using identifier[0]");
+			//			console.log("Using identifier[0]");
 						// This weill need changing, but no idea where to get the 3c2000 stuff from...
 						path = "http://cdn.loc.gov/service/pnp/cph/3c20000/3c24000/3c24100/" + imgUrl.substring(String(troveItem.identifier[0].value).length-7,String(troveItem.identifier[0].value).length);
 						console.log("Going to path " + path);
 					}
-				  loadedImages.push(path);
-				  imageYears.push(imgYear);
+				   imageData[0] = imgYear;
+		  		   imageData[1] =   path;
+				   imageData[2] =  troveItem.title;
+				   loadedImages.push(imageData);
+			console.log("Loaded images = " + imageData[0] + " and url=" + imageData[1]);
 	  
 			  }  else if (imgUrl.indexOf(urlPatterns[8]) >= 0) { // hdl.loc.gov
 	  
 				  found++;
 	//			  console.log(troveItem  );
-				  console.log("FOUND AN HDL LOC GOV: ");
+				//  console.log("FOUND AN HDL LOC GOV: ");
 		//		  console.log(troveItem.identifier[1].value);
 					if (troveItem.identifier.length > 1) {
 						path = troveItem.identifier[1].value;
 					} else {
 				  path = "http://cdn.loc.gov/service/pnp/cph/3c20000/3c24000/3c24100/" + imgUrl.substring(String(troveItem.identifier[0].value).length-7,String(troveItem.identifier[0].value).length) + "r.jpg";
 					}
-						console.log("Going to path " + path);
-				  loadedImages.push(
-	  
-					 path
-	  
-			  );
-				  imageYears.push(imgYear);
+		//				console.log("Going to path " + path);
+				   imageData[0] = imgYear;
+		  		   imageData[1] =   path;
+				   imageData[2] =  troveItem.title;
+				   loadedImages.push(imageData);
+			console.log("Loaded images = " + imageData[0] + " and url=" + imageData[1]);
 	  
 			  }
 	  
@@ -247,17 +189,27 @@
 	  
 				  // UNCOMMENT FOR DEBUG:
 	  
-			  // console.log("Not available: " + imgUrl);
+			// console.log("Not available: " + imgUrl);
 	  
 			  }
-		}
+	//	}
 
-
+        loadedImages.sort(sortFunction);
     }
 
 
+function sortFunction(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] < b[0]) ? -1 : 1;
+    }
+}
 
     function addFlickrItem(imgUrl, troveItem, imgYear) {
+     
+	    var imageData = [];
 
         var flickr_key = "a4d0bf2f4bde0595521b7bd8317ec428";
 
@@ -269,21 +221,17 @@
 
         var photo_id = url_comps[url_comps.length - 1];
 
-
-
         $.getJSON(flickr_url + photo_id + "&format=json&nojsoncallback=1", function(data) {
 
-            if (data.stat === "ok") {
+        if (data.stat === "ok") {
 
                 var flickr_image_url = data.sizes.size[data.sizes.size.length - 1].source;
 
-                loadedImages.push(
-
-                    flickr_image_url
-
-                );
-			imageYears.push(imgYear);
-
+				imageData[0] = imgYear;
+				imageData[1] = flickr_image_url;
+				imageData[2] =  troveItem.title;
+				loadedImages.push(imageData);
+			
             }
 
         });
@@ -296,37 +244,52 @@
 
     function printImages() {
 
-
-
-        $("#output").append("<h3>Image Search Results</h3>");
-
-
-
-	// Print out all images
-
-        for (var i in loadedImages) {
+		// Print out all images
+        console.log(loadedImages);
+		
+		// Create an empty html tag
+		var newHtml = $('');
+		
+        for (var i = 0; i < loadedImages.length; i++) {
 
             var image = new Image();
+				
+            image.src = loadedImages[i][1];
 
-            image.src = loadedImages[i];
+       //  image.style.display = "inline-block";
+     //     image.style.width = "30%";
+		//  image.style.margin = "1%";
+      //     image.style.verticalAlign = "top";
 
-            image.style.display = "inline-block";
+            image.class = "ui-widget-content";
 
-            image.style.width = "30%";
-
-            image.style.margin = "1%";
-
-            image.style.verticalAlign = "top";
+            image.alt = loadedImages[i][2];
+		
+			image.id = "draggable";	
 			
+			image.title = loadedImages[i][0];
 			
-            $("#output").append(image);
-			    $("#output").append(imageYears[i]);
+			image.setAttribute("draggable","true");
+			
+			image.setAttribute("ondragstart","drag(event)");		
+			
+// TODO Check whether image is loaded with jQuery
 
-        }
 
-
-
-    }
+	 //Handle the 'set of 6' image groupings
+		
+			if ((i % 6) === 0) {
+				//Commence by adding what we have to #output
+				$("#output").append(newHtml);
+				newHtml=$("<div class = 'img6'>");
+				//Add the first image to this img6 div
+				newHtml.append(image);
+			}else {
+				newHtml.append(image);
+			}
+		
+		 }
+	}
 
 
 
@@ -354,6 +317,12 @@
 
     }
 
-
-
     }(jQuery));
+
+     function succeed() {
+		  console.log("image loaded correctly"); 
+		  }
+
+     function fail() {
+		  console.log("image did not load"); 
+		  }
---
+++ page.php
@@ -0,0 +1,222 @@
+<!doctype html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
+  <script src="js/smartpaginator.js" type="text/javascript"></script>
+  <link href="css/smartpaginator.css" rel="stylesheet" type="text/css" />
+  <script src="js/jquery-ui-1.9.2.custom.min.js"></script>
+   <script>
+  $( function() {
+    $( "#draggable" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+   });
+  } );
+  </script>
+  <script type="text/javascript">
+     $(document).ready(function() {
+		   $('.img6.test:not(:first)').css('display', 'none');
+		   
+           $('#smart-paginator').smartpaginator({ totalrecords: 5, recordsperpage: 1, datacontainer: 'output', dataelement: '.img6', initval: 0, next: 'Next', prev: 'Prev', first: 'First', last: 'Last', theme: 'green'
+ 
+         });
+ 
+         function onChange(newPageValue) {
+             alert(newPageValue);
+		 }
+         });
+ </script>
+ 
+  <style>
+#draggable {
+	width: 200px;
+	height: 200px;
+	padding: 0.5em;
+	margin: 10px 10px 10px 0;
+}
+#droppable {
+	width: 200px;
+	height: 200px;
+	padding: 0.5em;
+	margin: 10px;
+}
+<!--
+img[title] {
+	color: red;
+	text-align: left
+}
+-->
+.ui-draggable, .ui-droppable {
+	background-position: top;
+}
+/* Set height of the grid so .sidenav can be 100% (adjust if needed) GF - this overrides teh stye.css .row.content*/
+.row.content {
+	height: 1500px
+}
+
+
+</style>
+  <title>Family Tree</title>
+ 
+  </head>
+
+  <body id="bootstrap-overrides">
+<div class="container-fluid">
+    <div class="row">
+    <div id= "right_container" class="col-sm-4">
+        <div class="container-fluid">
+        <div class="row">
+            <p class = "lead">Enter your surname here:</p>
+            <form action="#" id="searchTrove">
+            <input id="searchTerm" type="text" />
+            <button type="submit" id="searchbtn">Search</button>
+          </form>
+          
+          
+          
+            <h3>Image Search Results</h3>
+            
+          
+    <div id="smart-paginator" >  </div>
+          <div id="output">
+          <div class="img6">
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=102166" alt="Mcleannan Lola Ferguson and Alice, Paisley Scotland" id="draggable" title="1860" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=393808" alt="Johnny Ferguson - age 22 - &quot;Nallong Malbill&quot; Goulbourne [Goulburn] Tribe" id="draggable" title="1866" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=3361532" alt="Sir George Ferguson Bowen / J Botterill" id="draggable" title="1869" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=3494899" alt="Sir George Ferguson Bowen, Governor of Victoria, Australia. Bradley &amp; Rulofson" id="draggable" title="1873" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=3058791" alt="Helen Munro-Ferguson [wife of Governor General of Australia, 1814-1820]" id="draggable" title="1885" draggable="true" ondragstart="drag(event)">
+            <img src="http://nla.gov.au/nla.obj-145708093/representativeImage?wid=900" alt="Miss Ferguson" id="draggable" title="1890" draggable="true" ondragstart="drag(event)">
+        </div>
+          <div class="img6">
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=457553" alt="Wm. Ferguson, 1859 / G. L. Massingham" id="draggable" title="1896" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://collections.museumvictoria.com.au/content/media/19/719719-thumbnail.jpg" alt="Photograph - Portrait of 'Sister Ferguson', Kimberley, South Africa, 1902" id="draggable" title="1902" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=369926" alt="Senator J. Ferguson, Queensland / D. Bernard &amp; Co" id="draggable" title="1902" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://nla.gov.au/nla.obj-136668243/representativeImage?wid=900" alt="Portrait of Ronald Munro Ferguson / Alice Mills" id="draggable" title="1910" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://nla.gov.au/nla.obj-136668354/representativeImage?wid=900" alt="[Portrait of Sir Ronald Munro Ferguson]" id="draggable" title="1910" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://nla.gov.au/nla.obj-136668139/representativeImage?wid=900" alt="Portrait of Ronald Munro Ferguson, 1914-20" id="draggable" title="1910" draggable="true" ondragstart="drag(event)">
+            </div>
+          <div class="img6">
+            <img src="http://nla.gov.au/nla.obj-136668461/representativeImage?wid=900" alt="Portrait of  R. C. Munro Ferguson / H. Walter Barnett" id="draggable" title="1910" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=296917" alt="The Governor General Sir Ronald Munro-Ferguson in open carriage" id="draggable" title="1914" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=3128275" alt="Gnr. A.H. Ferguson, 42nd Bty. A.I.F" id="draggable" title="1915" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://cdn.loc.gov/service/pnp/cph/3c20000/3c24000/3c24100/3c12195r.jpg" alt="[Homer L. Ferguson, head-and-shoulders portrait, facing left]" id="draggable" title="1919" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=1273466" alt="[Neva Ferguson, Bus Conductor]" id="draggable" title="1927" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://nla.gov.au/nla.obj-136260767/representativeImage?wid=900" alt="Portrait of Sir John Ferguson / Monte Luke" id="draggable" title="1930" draggable="true" ondragstart="drag(event)">
+            </div>
+          <div class="img6">
+            <img src="http://collections.museumvictoria.com.au/content/media/48/482698-thumbnail.jpg" alt="Photograph - 'Portrait of D.B Ferguson', Melbourne, Victoria, circa 1932" id="draggable" title="1932" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://cdn.loc.gov/service/pnp/cph/3c20000/3c24000/3c24100/3c07083r.jpg" alt="[Elpidio Quirino, head-and-shoulders portrait, facing front] by staff correspondant Richard C. Ferguson" id="draggable" title="1949" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://nla.gov.au/nla.obj-151263763/representativeImage?wid=900" alt="Group portrait of, from left to right, George Ferguson of Angus and Robertson, Thelma Clune and William Dobell, ca. 1950s" id="draggable" title="1950" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://collections.museumvictoria.com.au/content/media/36/431886-thumbnail.jpg" alt="Photograph - Massey Ferguson, Group Portrait, Melbourne, Victoria, 1955-1965" id="draggable" title="1955" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://nla.gov.au/nla.obj-145873726/representativeImage?wid=900" alt="Portrait of Sir John Ferguson, ca. 1965 / Keast Burke" id="draggable" title="1959" draggable="true" ondragstart="drag(event)">
+            </div>
+          <div class="img6">
+            <img src="http://digital.slv.vic.gov.au/webclient/DeliveryManager?pid=2848945" alt="Sir John Ferguson" id="draggable" title="1960" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://collections.museumvictoria.com.au/content/media/19/432069-thumbnail.jpg" alt="Photograph - Massey Ferguson, Portrait of Ernie Nicholls, Australia, circa 1960s" id="draggable" title="1960" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://collections.museumvictoria.com.au/content/media/30/381680-thumbnail.jpg" alt="Photograph - Massey Ferguson, Cane Farmer, Queensland, 1960s" id="draggable" title="1960" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://collections.museumvictoria.com.au/content/media/35/431885-thumbnail.jpg" alt="Photograph - Massey Ferguson, Group Portrait, Melbourne, Victoria, 1960" id="draggable" title="1960" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://recordsearch.naa.gov.au/NAAMedia/ShowImage.asp?T=P&amp;S=1&amp;B=11354941" alt="Personalities - Portrait of Sir Ronald MUNRO-FERGUSON, former Governor General" id="draggable" title="1978" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://nla.gov.au/nla.obj-136335184/representativeImage?wid=900" alt="Portrait of Martin Ferguson, 1988" id="draggable" title="1988" draggable="true" ondragstart="drag(event)">
+            </div>
+          <div class="img6">
+            <img src="http://nla.gov.au/nla.obj-136519931/representativeImage?wid=900" alt="[Portrait of Martin Ferguson]" id="draggable" title="1990" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://nla.gov.au/nla.obj-136446944/representativeImage?wid=900" alt="Portrait of Senator Alan Ferguson taken at the Constitutional Convention, Canberra, February 2-13, 1998 / Loui Seselja" id="draggable" title="1998" draggable="true" ondragstart="drag(event)">
+            
+            <img src="http://nla.gov.au/nla.obj-146665056/representativeImage?wid=900" alt="Portrait of Martin Ferguson, Non-Executive member, National Library of Australia Council, 8 Oct. 1999 / Damian McDonald" id="draggable" title="1999" draggable="true" ondragstart="drag(event)">           
+            </div>
+          
+            </div>
+      </div>
+      </div>
+    <div class="col-sm-8"> 
+        <!--          			<div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>   -->
+        <section>
+        <div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)">
+            <form action="/upload" class="dropzone" id="dropzone">
+            <div class="fallback">
+                <input name="file" type="file" multiple/>
+              </div>
+          </form>
+          </div>
+        <div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)">
+            <form action="/upload" class="dropzone" id="dropzone">
+            <div class="fallback">
+                <input name="file" type="file" multiple/>
+              </div>
+          </form>
+          </div>
+      </section>
+        <section>
+        <div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)">
+            <form action="/upload" class="dropzone" id="dropzone">
+            <div class="fallback">
+                <input name="file" type="file" multiple/>
+              </div>
+          </form>
+          </div>
+      </section>
+      </div>
+  </div>
+  </div>
+<nav class="navbar navbar-fixed-bottom" id="navbar">
+<div class="container-fluid">
+    <ul class="nav navbar-inverse navbar-nav">
+    <li><a class="navbar-brand" href="index.php">Strengthen My DNA</a>
+      <li class="active"><a href="family_tree.php">Family Tree</a></li>
+    <li><a href="about.php">About</a></li>
+  </ul>
+    <ul class="nav navbar-nav navbar-right">
+    <li><a href="index.php"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+    <li><a href="index.php"><span class="glyphicon glyphicon-log-in"></span> Sign Out</a></li>
+  </ul>
+    </nav>
+  </div>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.min.js"></script> 
+
+<!--  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>  --> 
+<!--    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>   --> 
+<script src="js/trove.js"></script> 
+<script src="js/strengthen_dna_general.js"></script> 
+<script type="text/javascript" src="js/jquery.js?ver=1.11.1"></script> 
+<script type="text/javascript" src="js/jquery-migrate.min.js?ver=1.2.1"></script>
+</body>
+</html>
